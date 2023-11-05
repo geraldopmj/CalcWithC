c5cf1c1fdea1bb2f493201b0880cbf9f374d6a19
Geraldo PMJ <geraldo.pmj@gmail.com>
2023-11-05 12:01:59 -0300

    Create MedianCalc.c

---
+++ MedianCalc.c
@@ -0,0 +1,24 @@
+    int N, i, soma, media;
+    printf("Quantas notas para calcular a media?\n");
+    scanf("%i", &N);
+
+    int vetorNotas[N];
+
+    for (i = 0; i < N; i++)
+    {
+        printf("Digite uma nota:\n");
+        scanf("%i", &vetorNotas[i]);
+    }
+
+    soma = 0;
+
+    for (i = 0; i < N; i++)
+    {
+        soma = soma + vetorNotas[i];
+    }
+    media = soma / N;
+    printf("A media e %i\n", media);
+
+    printf("Programa executado com sucesso!\n");
+    return 0;
+}
